b45b90a77680d59386d64460736e0755f011ef90
Asebodi <aditraharditya45@gmail.com>
2020-02-12 13:14:45 +0700

    Implemented browser cookie for dark/light mode

--- provider.js
+++ provider.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react'
+import Cookies from 'js-cookie'
 
 export const myState = React.createContext()
 
 const Provider = props => {
   const [isDark, setTheme] = useState(false)
 
+  const getTheme = () => {
+    const getCookie = Cookies.get("isDark");
+
+    if (getCookie) {
+      setTheme(true)
+    } else {setTheme(false)}
+  };
+
+  React.useEffect(() => {
+    getTheme();
+  }, [])
+  
+
   return (
     <myState.Provider value={{
       isDark,
-      changeTheme: () => setTheme(!isDark)
+      changeTheme: () => {setTheme(!isDark); Cookies.set("isDark", {isDark}, {expires: 365})}
     }}>
       {props.children}
     </myState.Provider>
